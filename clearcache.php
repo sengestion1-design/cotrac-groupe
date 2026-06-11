1734097e6e9f5cffdc39c6aa5dee58afff35b62b
Mor Seck <mor.seck86@gmail.com>
2026-06-11 03:18:55 +0200

    fix(security): protéger clearcache.php par token

--- clearcache.php
+++ clearcache.php
@@ -1,4 +1,8 @@
 <?php
+if (!isset($_GET['token']) || !hash_equals('cotrac_clear_2026', $_GET['token'])) {
+    http_response_code(403);
+    exit('Accès refusé');
+}
 if (function_exists('opcache_reset')) {
     opcache_reset();
     echo 'OPcache vidé';
